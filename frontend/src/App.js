5fb0473bad3eff9e009fcaedcb8acb1b5a292e31
Stella Li <stellali0725@hotmail.com>
2023-06-08 16:59:22 -0400

    Add footer and header
    Added two folders for header and footer
    Need to install several new packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,15 @@ import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
 
 function App() {
-  
   return (
-     <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="navbar" element={<Navbar />} />
           <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
@@ -20,8 +22,8 @@ function App() {
 
           <Route path="login" element={<Login />} />
 
-          
           <Route path="*" element={<NoPage />} />
+          <Route path="footer" element={<Footer />} />
         </Route>
       </Routes>
     </BrowserRouter>
